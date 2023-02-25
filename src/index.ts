c5ac04f456e7ada77d7257f17411e3a723d09e4e
0x1000000 <25109784+0x1000000@users.noreply.github.com>
2023-02-25 13:49:09 -0500

    Explicit Export List

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
-export * from "./api/i_with_state"
-export * from "./api/with_state"
-export * from "./api/with_state_base"
-export * from "./api/state_tracking"
-export * from "./api/state_decorators/emitter"
-export * from "./api/state_decorators/output_for"
-export * from "./api/state_decorators/include_in_state"
-export * from "./api/state_decorators/bind_to_shared"
-export * from "./api/state_decorators/calc"
-export * from "./api/state_decorators/with"
-export * from "./api/state_decorators/with_async"
-export * from "./api/state_decorators/async_init"
-export * from "./api/state_decorators/with_shared_as_source"
-export * from "./api/state_decorators/with_shared_as_target"
+export { 
+    ComponentState,
+    ComponentStateDiff,
+    IStateHandler,
+    ISharedStateChangeSubscription,
+    StateTrackingOptions,
+    InitStateTrackingOptions,
+    initializeImmediateStateTracking,
+    initializeStateTracking,
+    releaseStateTracking,
+    StateTracking
+} from "./api/state_tracking"
+
+export { Emitter } from "./api/state_decorators/emitter"
+export { OutputFor } from "./api/state_decorators/output_for"
+export { IncludeInState } from "./api/state_decorators/include_in_state"
+export { BindToShared } from "./api/state_decorators/bind_to_shared"
+export { Calc } from "./api/state_decorators/calc"
+export { With } from "./api/state_decorators/with"
+export { WithAsync } from "./api/state_decorators/with_async"
+export { AsyncInit } from "./api/state_decorators/async_init"
+export { WithSharedAsSource, WithSharedAsSourceArg } from "./api/state_decorators/with_shared_as_source"
+export { WithSharedAsTarget, WithSharedAsTargetArg } from "./api/state_decorators/with_shared_as_target"
+
+//Think of marking as "deprecated"
+export { IWithState, IStateHolder } from "./api/i_with_state"
+export { WithState } from "./api/with_state"
+export { WithStateBase } from "./api/with_state_base"
